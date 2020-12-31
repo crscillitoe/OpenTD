97759cb3cd8086be1153c75ea86168ba7329c40f
Christian Scillitoe <christian.scillitoe@gmail.com>
2020-12-31 05:30:46 +0000

    Import coordinate

--- lib/game/gameBoard.ts
+++ lib/game/gameBoard.ts
@@ -1,15 +1,16 @@
 import { Level } from "../level/level";
 import { Tile } from "../level/tiles/tile";
 import { Renderable } from "../renderable";
+import { Coordinate } from "./coordinate";
 
 export class GameBoard {
-    private level: Level;
-    private tiles: Tile[][];
-    private renderables: Map<Coordinate, Renderable[]>;
+  private level: Level;
+  private tiles: Tile[][];
+  private renderables: Map<Coordinate, Renderable[]>;
 
-    constructor(level: Level) {
-        this.level = level;
-        this.tiles = level.getTiles();
-        this.renderables = new Map<Coordinate, Renderable[]>();
-    }
+  constructor(level: Level) {
+    this.level = level;
+    this.tiles = level.getTiles();
+    this.renderables = new Map<Coordinate, Renderable[]>();
+  }
 }
