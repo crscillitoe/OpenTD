1ec9aaa95d34bfbfab2ccd251c18078580812574
Christian Scillitoe <christian.scillitoe@gmail.com>
2020-12-31 16:58:26 +0000

    Scaffold up angular material dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,21 @@ import { TowerComponent } from './editor/tower/tower.component';
 import { EnemyComponent } from './editor/enemy/enemy.component';
 import { LevelComponent } from './editor/level/level.component';
 import { EditorComponent } from './editor/editor.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -14,13 +29,28 @@ import { EditorComponent } from './editor/editor.component';
     TowerComponent,
     EnemyComponent,
     LevelComponent,
-    EditorComponent
+    EditorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatCardModule,
+    MatSlideToggleModule,
+    MatTableModule,
+    MatTooltipModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
